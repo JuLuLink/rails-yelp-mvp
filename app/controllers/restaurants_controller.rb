23cf3b24ab015ceb41930ad0866d99246c333817
Julien LUCIANI <korstg@gmail.com>
2019-02-13 17:16:27 +0100

    add view restaurant / review, update _index.scss, restaurants_controller, review.rb, routes.rb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,22 +1,46 @@
 class RestaurantsController < ApplicationController
-  def index         # GET /restaurants
+  # before_action :find_restaurant
+
+  def index
+    # GET /restaurants
+    @restaurants = Restaurant.all
   end
 
-  def show          # GET /restaurants/:id
+  def show
+    # GET /restaurants/:id
+    @restaurant = Restaurant.find(params[:id])
   end
 
-  def new           # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+    # GET /restaurants/new
   end
 
-  def create        # POST /restaurants
+  def create
+    # POST /restaurants
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
   end
 
-  def edit          # GET /restaurants/:id/edit
-  end
+  # def edit          # GET /restaurants/:id/edit
+  # end
 
-  def update        # PATCH /restaurants/:id
-  end
+  # def update        # PATCH /restaurants/:id
+  # end
 
-  def destroy       # DELETE /restaurants/:id
+  # def destroy       # DELETE /restaurants/:id
+  # end
+
+  private
+
+  # def find_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  def restaurant_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
